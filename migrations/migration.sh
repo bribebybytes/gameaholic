d516c4b60dce9b3c2de2dfa2a1a597ddcfaddcdc
Natsai <sobitha.natsai@adp.com>
2020-03-20 23:44:38 +0530

    fixing docker-compose issue on linux

--- migrations/migration.sh
+++ migrations/migration.sh
@@ -2,13 +2,13 @@
 #set -e
 echo "Sleep for 1 min..."
 sleep 60
-if [[ $(mysql -proot -e 'show databases;' | grep gameaholic | wc -l) == 0 ]];
+if [[ $(mysql -hgameaholic_mysql -proot -e 'show databases;' | grep gameaholic | wc -l) == 0 ]];
 then
 echo "Creating database..."
-mysql -proot -e 'create database gameaholic;'
+mysql -hgameaholic_mysql -proot -e 'create database gameaholic;'
 sleep 1
 echo "Restore database..."
-mysql -proot gameaholic < /var/run/mysqld/*_create_bootstraps.mysql.up.sql
+mysql -hgameaholic_mysql -proot gameaholic < /var/run/mysqld/*_create_bootstraps.mysql.up.sql
 else
 echo "database already exist.."
 exit 0
